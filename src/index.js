9af37968e95c68f2c4b9736b52a528e9b2ae6c7f
robslawinski <rslawinski@verizon.net>
2018-05-05 15:23:49 -0400

    Index Page Set Up

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import rootReducer from './reducers'
+import App from './components/App'
+​
+const store = createStore(rootReducer)
+​
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
